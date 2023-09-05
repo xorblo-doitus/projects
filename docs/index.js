4642326406b02a46163f397e5b7773fab45cfcc1
xorblo-doitus <blabla86420@gmail.com>
2023-09-05 17:51:03 +0200

    First sorting draft

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,45 @@
 import { CSVParser } from "./lib/patou/csv-parser/csv-parser.js";
 import { translationServer } from "./common/common.js";
+import { Sorter } from "./lib/patou/sort/sort.js";
+import { ProjectCard } from "./elements/define-elements.js";
+
+const PROJECT_LIST = document.getElementById("project-list");
+
+class ProjectSorter extends Sorter {
+    // includedTags = ["arcade"]; // Testing values.
+    // excludedTags = ["multiplayer"]; // Testing values.
+    
+    /**
+     * @param {ProjectCard} element 
+     * @returns {String[]}
+     */
+    getTags(element) { return [...element.tagsList.values()] }
+    
+    /**
+	 * @returns {ProjectCard[]}
+	 */
+	getElements() {
+        const result = [];
+        for (const childNode of PROJECT_LIST.childNodes) {
+            if (childNode instanceof ProjectCard) {
+                result.push(childNode);
+            }
+        }
+		return result;
+	}
+    
+    /**
+     * @param {ProjectCard[]} newOrder 
+     */
+    onSorted(newOrder) {
+        for (const element of this.lastDropped) {
+            element.style.display = "none";
+        }
+        for (const [idx, element] of newOrder.entries()) {
+            element.style.order = idx;
+        }
+    }
+}
 
 var text = (await fetch("assets/data/projects.csv").then((response) => {return response.text()}));
 
@@ -19,8 +59,11 @@ for (const row of parser) {
     newCard.setAttribute("thumbnail", row.get("thumbnail"));
     newCard.setAttribute("url", row.get("url"));
     newCard.setAttribute("tags", row.get("tags"));
-    document.getElementById("project-list").appendChild(newCard);
+    PROJECT_LIST.appendChild(newCard);
 }
 // end = performance.now();
 
 // console.log(`Iteration took ${end - start} milliseconds`)
+
+
+new ProjectSorter().sort();
---
+++ docs/lib/patou/sort/sort.js
@@ -0,0 +1,156 @@
+class Sorter {
+	/**
+	 * Check elements against these callables, if any return true, it will be included.
+	 * @type {Array<function(*): Boolean>} (element) => {if (should be shown) {return true} else {return false}}
+	 */
+	whiteList = [];
+	/**
+	 * Check elements against these callables, if any return false, it will be excluded, even if it mathed a whitelister.
+	 * @type {Array<function(*): Boolean>} (element) => {if (should NOT be shown) {return true} else {return false}}
+	 */
+	blackList = [];
+	
+	/**
+	 * When sorting, elements with these tags will be shown.
+	 * @type {String[]}
+	 */
+	includedTags = [];
+	
+	/**
+	 * When sorting, elements with these tags will NOT be showen.
+	 * @type {String[]}
+	 */
+	excludedTags = [];
+	
+	/**
+	 * @virtual
+	 * @param {*} element 
+	 * @returns {String[]}
+	 */
+	getTags(element) {
+		return [];
+	}
+	
+	/**
+	 * @virtual
+	 */
+	getElements() {
+		return [];
+	}
+	
+	/**
+	 * Called at the end of sort().
+	 * @param {*[]} newOrder 
+	 */
+	onSorted(newOrder) {
+		
+	}
+	
+	/** @type {*[]} */
+	lastResult = [];
+	/** @type {*[]} */
+	lastDropped = [];
+	
+	/**
+	 * Sort elements in a new order.
+	 * @param {?Array} elements If null, fetch with getElements().
+	 * @returns {Array}
+	 */
+	sort(elements = null) {
+		if (elements == null) {
+			elements = this.getElements();
+		}
+		
+		let result = [];
+		
+		// White List
+		let dropped = [];
+		if (this.whiteList.length != 0) {
+			for (const element of elements) {
+				if (this.matchAny(element, this.whiteList)) {
+					result.push(element);
+				} else {
+					dropped.push(element)
+				}
+			}
+		} else if (this.includedTags.length == 0) {
+			result = elements;
+		} else {
+			dropped = elements;
+		}
+		
+		// Included Tags
+		if (this.includedTags.length != 0) {
+			for (const element of dropped) {
+				if (this.matchAnyTag(this.getTags(element), this.includedTags)) {
+					result.push(element);
+				}
+			}
+		}
+		
+		
+		// Black List
+		if (this.blackList.length != 0) {
+			let newResult = [];
+			for (const element of result) {
+				if (!this.matchAny(element, this.blackList)) {
+					newResult.push(element);
+				}
+			}
+			result = newResult;
+		}
+		
+		// Excluded tags
+		if (this.excludedTags.length != 0) {
+			let newResult = [];
+			for (const element of result) {
+				if (!this.matchAnyTag(this.getTags(element), this.excludedTags)) {
+					newResult.push(element);
+				}
+			}
+			result = newResult;
+		}
+		
+		
+		this.lastResult = result;
+		this.lastDropped = elements.reduce(
+			(accumulator, newValue) => {
+				if (!result.includes(newValue)) {
+					accumulator.push(newValue);
+				}
+				return accumulator;
+			},
+			[]
+		);
+		
+		this.onSorted(result);
+		return result;
+	}
+	
+	/**
+	 * @param {*} element 
+	 * @param {Array<function(*): Boolean>} checkers 
+	 */
+	matchAny(element, checkers) {
+		for (const checker of checkers) {
+			if (checker(element)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * @param {String[]} elementTags 
+	 * @param {String[]} filterTags 
+	 */
+	matchAnyTag(elementTags, filterTags) {
+		return elementTags.some(
+			(tag) => {return filterTags.includes(tag);}
+		);
+	}
+}
+
+
+
+export { Sorter }
