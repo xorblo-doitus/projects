8e775be0371250987167dba5c47654bd39122da6
xorblo-doitus <blabla86420@gmail.com>
2023-10-23 10:47:00 +0200

    Implemented string query in `Sorter`.

--- docs/lib/patou/sort/sort.js
+++ docs/lib/patou/sort/sort.js
@@ -1,3 +1,133 @@
+class FuzzySearch {
+	/**
+	 * @param {object[]} toScan 
+	 * @param {object} options WARNING : you can't modify it afterwards. (At least you can't for key name).
+	 */
+	constructor(toScan, options) {
+		for (const key of options.keys) {
+			if (!key.keyPath) {
+				key.keyPath = key.name.split(".");
+			}
+		}
+		
+		this.toScan = toScan;
+		this.options = options;
+	}
+	
+	static getScore(queryPart, part) {
+		if (queryPart == part) {
+			return queryPart.length * 1.6 + 1;
+		}
+		
+		if (part.includes(queryPart)) {
+			return queryPart.length;
+		}
+		
+		let score = 0;
+		let idx = 0;
+		let queryIdx = 0;
+		let len = part.length
+		let lenQ = queryPart.length
+		
+		while (idx < len && queryIdx < lenQ) {
+			switch (part[idx]) {
+				case queryPart[queryIdx]:
+					score++;
+					queryIdx++;
+					break;
+				case queryPart[queryIdx + 1]:
+					if (part[idx + 1] == queryPart[queryIdx]) {
+						score += 1.5;
+						idx++;
+						queryIdx += 2;
+					} else {
+						score += 0.5;
+						queryIdx += 2;
+					}
+					break;
+				case queryPart[queryIdx - 1]:
+					score += 0.5;
+					queryIdx--;
+					break;
+				default:
+					score -= 0.1;
+			}
+			idx++;
+		}
+		
+		return Math.max(score, 0);
+	}
+	
+	/**
+	 * 
+	 * @param {*} obj 
+	 * @param {PropertyKey[]} keyPath 
+	 * @returns 
+	 */
+	static getByKeyPath(obj, keyPath) {
+		return keyPath.reduce(
+			function(a,b) {
+				return a && a[b];
+			},
+			obj
+		);
+	}
+	
+	static compareScores(a, b) {
+		return b.score - a.score;
+	}
+	
+	/**
+	 * 
+	 * @param {string} query 
+	 */
+	search(query) {
+		const keys = this.options.keys;
+		const splittedQuery = query.toLowerCase().split(" ");
+		const scores = [];
+		
+		for (const toScan of this.toScan) {
+			let score = 0;
+			for (const {keyPath, weight} of this.options.keys) {
+				let subScore = 0;
+				for (const part of FuzzySearch.getByKeyPath(toScan, keyPath).toLowerCase().split(" ")) {
+					for (const queryPart of splittedQuery) {
+						subScore += FuzzySearch.getScore(queryPart, part);
+					}
+				}
+				score += subScore * (weight || 1);
+			}
+			scores.push({
+				item: toScan,
+				score: score,
+			})
+		}
+		
+		scores.sort(FuzzySearch.compareScores);
+		
+		
+		const threshold = splittedQuery.reduce(
+			function (accumulator, value) {
+				return accumulator + value.length;
+			},
+			0
+		) * this.options.threshold || 0;
+		
+		console.log(threshold, " - max: ", scores[0].score);
+			
+		const result = [];
+		for (const scoreItem of scores) {
+			if (scoreItem.score < threshold) {
+				break;
+			}
+			result.push(scoreItem);
+		}
+		
+		return result;
+	}
+}
+
+
 class Sorter {
 	/**
 	 * @type {Map<String, function(*, *): Number>}
@@ -35,6 +165,27 @@ class Sorter {
 	 */
 	excludedTags = [];
 	
+	fuseOptions = {};
+	
+	query = "";
+	
+	_elements = null;
+	
+	/**
+	 * @param {object[]} newValue
+	 */
+	set elements(newValue) {
+		this._elements = newValue;
+		this._fuse = new Fuse(newValue, this.fuseOptions);
+	}
+	get elements() {
+		if (this._elements == null) {
+			this._elements = this.getElements();
+			this._fuse = new FuzzySearch(this._elements, this.fuseOptions);
+		}
+		return this._elements;
+	}
+	
 	/**
 	 * @virtual
 	 * @param {*} element 
@@ -71,20 +222,31 @@ class Sorter {
 	 */
 	sort(elements = null) {
 		if (elements == null) {
-			elements = this.getElements();
+			elements = this.elements;
+		} else {
+			this.elements = elements;
 		}
 		
 		let result = [];
 		
-		// White List
-		if (this.whiteList.length != 0) {
-			for (const element of elements) {
-				if (this.matchEvery(element, this.whiteList)) {
-					result.push(element);
-				}
+		if (this.query) {
+			console.dir(this._fuse.search(this.query));
+			for (const elem of this._fuse.search(this.query)) {
+				result.push(elem.item);
 			}
 		} else {
 			result = elements;
+		}
+		
+		// White List
+		if (this.whiteList.length != 0) {
+			let newResult = [];
+			for (const element of result) {
+				if (this.matchEvery(element, this.whiteList)) {
+					newResult.push(element);
+				}
+			}
+			result = newResult;
 		}
 		
 		// Included Tags
@@ -121,14 +283,12 @@ class Sorter {
 			result = newResult;
 		}
 		
-		
 		if (this.comparingFunctions.get(this.currentComparingFunction, undefined) != undefined) {
 			result.sort(this.comparingFunctions.get(this.currentComparingFunction));
+			if (this.reverted) {
+				result.reverse();
+			}
 		}
-		if (this.reverted) {
-			result.reverse();
-		}
-		
 		
 		this.lastResult = result;
 		this.lastDropped = elements.reduce(
